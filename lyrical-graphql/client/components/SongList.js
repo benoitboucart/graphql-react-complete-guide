13dafd6691422368e8032adc459cc48ddc696ce6
Benoît Boucart <benoit@webbb.be>
2018-08-23 15:52:25 +0200

    React component design

---
+++ lyrical-graphql/client/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+  render = () => {
+    return (
+      <div>
+        ddSonglist
+      </div>
+    );
+  }
+}
+
+export default SongList;
