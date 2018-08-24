57f8da314d1c289cf516dc633cad3db5a3125a8c
Benoît Boucart <benoit@webbb.be>
2018-08-24 11:08:39 +0200

    Make dev server historyCallback & proxy

--- lyrical-graphql/webpack.config.js
+++ lyrical-graphql/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -6,14 +7,19 @@ module.exports = {
   entry: './client/index.js',
   devServer: {
     port: 8080,
-    open: true,
-    historyApiFallback: true
-    // proxy: {
-    //   "/api": "http://localhost:8080"
-    // }
+    historyApiFallback: {
+      // Paths with dots should still use the history fallback.
+      // See https://github.com/facebookincubator/create-react-app/issues/387.
+      disableDotRule: true,
+    },
+    proxy: {
+      // make sure we have a proxy for GraphQL requests
+      "/graphql": "http://localhost:4000/graphql"
+    }
   },
   output: {
-    path: '/',
+    path: path.resolve(__dirname, '/'),
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
