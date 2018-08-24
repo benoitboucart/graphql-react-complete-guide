29c3d4cb2356c2ccc92531cabe2b32c74c5c3d93
Benoît Boucart <benoit@webbb.be>
2018-08-24 11:55:44 +0200

    Mutations in React Section 8, Lecture 44

--- lyrical-graphql/client/components/SongCreate.js
+++ lyrical-graphql/client/components/SongCreate.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+const ADD_SONG = gql`
+  mutation AddSong($title: String!) {
+    addSong(title: $title){
+      id,
+      title
+    }
+  }
+`;
 
 class SongCreate extends Component {
-  state = {
-    title: ``
-  }
+  // state = {
+  //   title: ``
+  // };
 
   render = () => {
     return (
-      <div>
-        <h3>Create a new song</h3>
-        <form>
-          <label>Song title</label>
-          <input
-            value={this.state.title}
-            onChange={e => this.setState({ title: event.target.value })}
-          />
-          {this.state.title}
-        </form>
-      </div>
+
+      <Mutation mutation={ADD_SONG}>
+        {(addSong, { data }) => (
+          <div>
+            <h3>Create a new song</h3>
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                addSong({ variables: { title: this.input.value } });
+                this.input.value = "";
+              }}
+            >
+              <input
+                ref={node => {
+                  this.input = node;
+                }}
+              />
+              <button type="submit">Add Todo</button>
+            </form>
+          </div>
+        )}
+
+        {/* <div>
+          <form onSubmit={this.onSubmit}>
+            <label>Song title</label>
+            <input
+              value={this.state.title}
+              onChange={e => this.setState({ title: e.target.value })}
+            />
+            {this.state.title}
+          </form>
+        </div> */}
+      </Mutation>
     );
   }
 };
