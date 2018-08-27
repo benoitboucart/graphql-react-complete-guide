2086614326aab1f8cd866619056be4aa0092f811
Benoît Boucart <benoit@webbb.be>
2018-08-27 15:14:16 +0200

    webpack & server setup

--- auth-graphql/server/server.js
+++ auth-graphql/server/server.js
@@ -60,4 +60,8 @@ const webpack = require('webpack');
 const webpackConfig = require('../webpack.config.js');
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
+app.listen(4000, () => {
+  console.log('Listening');
+});
+
 module.exports = app;
--- auth-graphql/webpack.config.js
+++ auth-graphql/webpack.config.js
@@ -1,10 +1,25 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './client/index.js',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      // Paths with dots should still use the history fallback.
+      // See https://github.com/facebookincubator/create-react-app/issues/387.
+      disableDotRule: true,
+    },
+    proxy: {
+      // make sure we have a proxy for GraphQL requests
+      "/graphql": "http://localhost:4000/graphql"
+    }
+  },
   output: {
-    path: '/',
+    path: path.resolve(__dirname, '/'),
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
