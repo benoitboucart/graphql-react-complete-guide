bef311f0e004e60e65fbc247bd9e0ee26eeb90e2
Benoît Boucart <benoit@webbb.be>
2018-08-27 15:16:29 +0200

    No webpackMiddleware

--- auth-graphql/server/server.js
+++ auth-graphql/server/server.js
@@ -55,10 +55,10 @@ app.use('/graphql', expressGraphQL({
 // Webpack runs as a middleware.  If any request comes in for the root route ('/')
 // Webpack will respond with the output of the webpack process: an HTML file and
 // a single bundle.js output of all of our client side Javascript
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('../webpack.config.js');
+// app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(4000, () => {
   console.log('Listening');
