a73a5e57656e907b9b90db5cf10727478788a1b1
Benoît Boucart <benoit@webbb.be>
2018-08-27 14:16:05 +0200

    68. The Like Mutation

--- lyrical-graphql/client/components/LyricList.js
+++ lyrical-graphql/client/components/LyricList.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 class LyricList extends Component {
 
+  onLike = id => {
+    console.log([this.onLike, id])
+  };
+
   renderLyrics = () => {
     return (
       this.props.lyrics.map(({ id, content }) => (
         <li key={id} className="collection-item">
           {content}
+
+          <i
+            onClick={() => this.onLike(id)}
+            className="material-icons"
+          >
+            thumb_up
+          </i>
         </li>
       ))
     );
